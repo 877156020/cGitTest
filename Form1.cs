9f599a0e293d0769578ef9136f491025b2f00e90
877156020 <877156020@qq.com>
2024-08-20 22:54:47 +0800

    增加弹出框

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace cGitTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("黑神话-悟空1");
+        }
     }
 }
